a9736717c55c0575b86d4f26b90d05a07cab07f6
ArkarPhoneMyat <r.karphonemyat@gmail.com>
2023-01-13 20:57:20 +0630

    Update NavBar.js

--- src/Pages/components/NavBar.js
+++ src/Pages/components/NavBar.js
@@ -8,23 +8,17 @@ import { images, myColor, sizes } from '../../constants';
 export default function Navbar() {
   return (
     <Box sx={{ flex: 1, display: 'flex',alignItems: 'center' }}>
-      
-          
-          <Typography variant="h3" component="div" sx={{flex: 1, textAlign: 'center', color: 'black' }}>
-            flymya
-          </Typography>
-          <Box sx={{flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: 'red'}}></Box>
-          
-          <Box sx={{flex: 1, textAlign: 'center'}}>
-          <Box sx={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap'}}>
+      <Typography variant="h3" component="div" sx={{flex: 1, textAlign: 'center', color: 'black' }}>
+        flymya
+      </Typography>
+      <Box sx={{flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: 'red'}}></Box>
+      <Box sx={{flex: 1, textAlign: 'center'}}>
+        <Box sx={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap'}}>
           <Styled.BlogP color={myColor.black} size={sizes.h3}>Blog</Styled.BlogP>
           <Styled.FlagImg src={images.britishFlag}/>
           <SvgIcon component={AccountCircleOutlinedIcon} sx={{width: '40px', height: '40px', paddinTop: 20}}></SvgIcon>
-          </Box>
-          
-          </Box>
-          
-        
+        </Box>
+      </Box>
     </Box>
   )
 }
